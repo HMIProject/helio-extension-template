641e0038331d4d1d5bdf881f8c6cc04e617c3d2a
Philipp Kruse <kruse@hmi-project.com>
2025-11-24 11:37:29 +0100

    Improve file name generation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,9 +4,19 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 
+// eslint-disable-next-line no-undef
+const { npm_package_name: name, npm_package_version: version } = process.env;
+
+if (name === 'helio-extension-template') {
+  console.log(`\x1b[43mDon't forget to define your package name in 'package.json'.
+Still using the default: '${name}'.\x1b[0m`);
+}
+
 export default {
   input: 'src/main.tsx',
-  output: { file: 'lib/my-extension.js' },
+  output: {
+    file: `lib/${name}-${version}.js`,
+  },
 
   external: ['react', 'react/jsx-runtime', '@hmiproject/helio-sdk'],
 
