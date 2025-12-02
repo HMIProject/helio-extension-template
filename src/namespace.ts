210f68f3ff7b63048a1fc56c2123b518b0c006a0
Philipp Kruse <kruse@hmi-project.com>
2025-12-02 12:50:32 +0100

    Add set of examples

--- src/namespace.ts
+++ src/namespace.ts
@@ -1,5 +1,7 @@
 import { createNamespace } from '@hmiproject/helio-sdk';
 
 export const namespace = createNamespace({
+  // Change this so it matches your company's domain name in
+  // https://en.wikipedia.org/wiki/Reverse_domain_name_notation
   name: 'com.my-company.my-extension',
 });
